842b86ba250661a9eee1c12c73a71b89410eef79
vari <bryan_99yong@hotmail.com>
2023-03-13 11:01:52 -0500

    update130320231101

--- src/components/common/header/Head.jsx
+++ src/components/common/header/Head.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import image from "../images/logosf.png";
+// import image from "../images/encabecadodisenio.png";
 import image1 from "../images/direprologo.png";
 const Head = () => {
   return ( <> <section className='head'>
@@ -8,10 +8,10 @@ const Head = () => {
           <img src={image1} />
               {/* <h1>hellooooooooo</h1> */}
           </div>
-          <div className='ad'>
+          {/* <div className='ad'>
               <img src={image} alt='' />
-              {/* <h1>hellooooooooo</h1> */}
-          </div>
+              
+          </div> */}
           
           
   </div>
